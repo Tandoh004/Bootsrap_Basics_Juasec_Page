06c8d9095e696ef4bb010aad154b9d249ed6ee69
Tandoh004 <atandoh004@st.ug.edu.gh>
2023-07-14 19:41:58 +0000

    js changes

--- myJsScript.js
+++ myJsScript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function myfunc()
 {document.getElementById("doc").innerHTML = "GREAT JUASEC";
 
@@ -16,23 +15,4 @@ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 })
 
 
-=======
-function myfunc()
-{document.getElementById("doc").innerHTML = "GREAT JUASEC";
-
-document.getElementById("doc2").innerHTML = "DWENEKYEA BIE"
-
-}
-
-
-
-
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
-
-
->>>>>>> fdd65e9e13b0ff191f2c8c1189a6076cba47e814
 //document.getElementById("par").innerHTML = "We are the boldest"//
